4b2e25f56ee0b17299e69d10fdd4e17b533f98e9
taeto18 <36634558+taeto18@users.noreply.github.com>
2018-05-25 16:32:19 +0700

    init project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+  public function show()
+  {
+    $taya="Sample controller";
+    $para="1";
+    return $taya;
+  }
+}
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+//use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//
+// Route::get('to', function () {
+//     return view('to');
+// });
+//
+// Route::get('new' ,function () {
+//   return "Hello Tayakorn";
+// });
+//
+// //Route::view('/','new');
+// Route::get('new/{id}',function($id){
+//   return $id;
+// });
+//
+// Route::get('new/{ild}/{ll}',function(Request $request, $gg,$ild){
+//   return $request->all();
+// });
+//
+// Route::redirect('/old','/blog/public/new',301);
+//
+// Route::get('test','UserController@show');
+//
+// Route::get('/taeto/{name}',function($name){
+//   return view('taeto',['name'=>$name]);
+// });
+
+Route::get('/',function(){
+  return view('block,block');
+});
