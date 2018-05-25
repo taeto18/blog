d8b1535af0780d7318c4c6d891d719e72be522cb
taeto18 <36634558+taeto18@users.noreply.github.com>
2018-05-25 18:52:54 +0700

    rr

--- app/Http/Controllers/line.php
+++ app/Http/Controllers/line.php
@@ -9,19 +9,20 @@ class lineController extends Controller
 {
     public function webhook(Request $request)
     {
-        $type = $request['events'][0]['type'] ??'';
-        $userid = $request['events'][0]['source']['userId'];
-        $replyToken = $request['events'][0]['replyToken'] ??'';
-        $message =$request['events']['message']['text'];
+        $type = $request['events'][0]['type'] ?? '';
+        $userId = $request['events'][0]['source']['userId'];
+        $replyToken = $request['events'][0]['replyToken'] ?? '';
+        $message = $request['events']['message']['text'] ?? 'Hello';
 
         if ($type == 'message') {
-            $this->replyfunction($replyToken,$message);
+            $this->replyfunction($replyToken, $message);
         }
 
         return 'to';
     }
 
-    public function replyfunction($replyToken, $message) {
+    public function replyfunction($replyToken, $message)
+    {
         $accessToken = 'LHRTu0vJ5vkMRL+SSp3i2zPioJe8FRavHEB0h6gJnHL+AvmzBChUd2c3mc6rFTTXQdl5COpkxFSyqPfJBvOD03x0apVBvI2z9BfgUN/y0fqW+omUOFyHOM6wNZ5CaOeYwUe0KB0SY8ysd9iqc1qwigdB04t89/1O/w1cDnyilFU=';
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
         $bot = new \LINE\LINEBot($httpClient, ['b2eb356d2c894187569868645146c85c' => ($httpClient)]);
