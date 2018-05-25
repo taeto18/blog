c89510ad231bdfb304e6c468dedcbe5650385705
taeto18 <36634558+taeto18@users.noreply.github.com>
2018-05-25 19:31:09 +0700

    ...

--- app/Http/Controllers/LineApiController.php
+++ app/Http/Controllers/LineApiController.php
@@ -23,7 +23,7 @@ class LineApiController extends Controller
     {
         $accessToken = 'LHRTu0vJ5vkMRL+SSp3i2zPioJe8FRavHEB0h6gJnHL+AvmzBChUd2c3mc6rFTTXQdl5COpkxFSyqPfJBvOD03x0apVBvI2z9BfgUN/y0fqW+omUOFyHOM6wNZ5CaOeYwUe0KB0SY8ysd9iqc1qwigdB04t89/1O/w1cDnyilFU=';
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($accessToken);
-        $bot = new \LINE\LINEBot($httpClient, ['b2eb356d2c894187569868645146c85c' => ($httpClient)]);
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'b2eb356d2c894187569868645146c85c']);
 
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
